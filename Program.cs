a96313be06bc219f87e774fce4204d30ad120f84
gabynichifor <74709531+gabynichifor@users.noreply.github.com>
2023-11-01 15:28:21 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,58 +1,36 @@
 ﻿using System;
 //exercitiu 1
+
 class Program
 {
     static void Main(string[] args)
     {
-        int number;
-
-        do
+        Console.WriteLine("Introduceti un numar de 3 cifre: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        while (number < 100)
         {
-            Console.Write("Introdu un nr de min. 3 cifre: ");
-            string userInput = Console.ReadLine();
-
-            if (int.TryParse(userInput, out number) && userInput.Length >= 3)
-            {
-                // Va calcula oglinda numărului
-                int reversedNumber = ReverseNumber(number);
-
-                Console.WriteLine($"Oglinda numărului este: {reversedNumber}");
-
-                // Verifică dacă oglinda este pătrat perfect
-                if (IsPerfectSquare(reversedNumber))
-                {
-                    Console.WriteLine("Oglinda numărului este pătrat perfect.");
-                }
-                else
-                {
-                    Console.WriteLine("Oglinda numărului nu este pătrat perfect.");
-                }
-
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Numărul introdus nu are 3 cifre. Introdu alt număr.");
-            }
-        } while (true);
-    }
-
-    static int ReverseNumber(int num)
-    {
-        int reversedNum = 0;
-        while (num > 0)
-        {
-            int digit = num % 10;
-            reversedNum = reversedNum * 10 + digit;
-            num /= 10;
+            Console.WriteLine("Nr introdus nu este de 3 cifre. Introduceti din nou: ");
+            number = Convert.ToInt32(Console.ReadLine());
         }
-        return reversedNum;
-    }
 
-    static bool IsPerfectSquare(int num)
-    {
-        int sqrt = (int)Math.Sqrt(num);
-        return sqrt * sqrt == num;
+        int reversedNumber = 0;
+        while (number != 0)
+        {
+            int lastdigit = number % 10;
+            reversedNumber = reversedNumber * 10 + lastdigit;
+            number /= 10;
+        }
+
+      int reversedNumSqrt = reversedNumber(Math.Sqrt) ;
+        if (reversedNumSqrt * reversedNumSqrt == reversedNumber)
+        {
+            Console.WriteLine("Nr inversat este patrat perfect");
+        }
+        else
+        {
+            Console.WriteLine("Nr inversat nu este patrat perfect");
+        }
+     
+           
     }
 }
-
