6bc87ca37cb7e97174586b7f7fa730db74e7d6d6
gabynichifor <74709531+gabynichifor@users.noreply.github.com>
2023-11-01 16:34:47 +0200

    Update Program2.cs

--- Program2.cs
+++ Program2.cs
@@ -1,61 +1,23 @@
-using System;
-//Exercitiu 2
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.Write("Introdu o listă de numere: ");
-        string input = Console.ReadLine();
-        string[] numbersAsString = input.Split(' ');
-
-        double[] numbers = new double[numbersAsString.Length];
-
-        // Conversia și salvarea numerelor în array
-        for (int i = 0; i < numbersAsString.Length; i++)
+// 3. Afisarea numerelor care nu sunt întregi
+        Console.WriteLine("Numerele care nu sunt întregi sunt:");
+        for (int i = 0; i < numbersDouble.Length; i++)
         {
-            if (double.TryParse(numbersAsString[i], out double num))
+            if (numbersDouble[i] != numbersInt[i])
             {
-                numbers[i] = num;
-            }
-            else
-            {
-                Console.WriteLine($"Elementul '{numbersAsString[i]}' este număr nevalid.");
+                Console.WriteLine(numbersDouble[i]);
             }
         }
 
-        Console.WriteLine("Numere ce nu sunt întregi:");
-
-        // Afișarea numerelor care nu sunt întregi
-        foreach (var num in numbers)
+        // 4. Găsirea și afișarea celui mai mic număr fara a folosi funcții din .NET
+        Console.WriteLine("Cel mai mic număr fara a folosi funcții din .NET este:");
+        double minNumber = numbersDouble[0];
+        for (int i = 1; i < numbersDouble.Length; i++)
         {
-            if (!IsInteger(num))
+            if (numbersDouble[i] < minNumber)
             {
-                Console.WriteLine(num);
+                minNumber = numbersDouble[i];
             }
         }
-
-        // Cautarea și afișarea celui mai mic număr
-        double smallestNumber = FindSmallestNumber(numbers);
-        Console.WriteLine($"Cel mai mic număr este: {smallestNumber}");
-    }
-
-    static bool IsInteger(double num)
-    {
-        return num == Math.Floor(num);
-    }
-
-    static double FindSmallestNumber(double[] numbers)
-    {
-        double smallest = double.MaxValue;
-
-        foreach (var num in numbers)
-        {
-            if (num < smallest)
-            {
-                smallest = num;
-            }
-        }
-
-        return smallest;
+        Console.WriteLine(minNumber);
     }
 }
